1f94a8b498b6fefecfc836e3ceff5ecf02fa4823
Constantin Pape <c.pape@gmx.net>
2017-08-14 23:31:45 -0400

    build for all python versions

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -1,4 +1,4 @@
 mkdir -p ${PREFIX}/volumina_viewer
 cp volumina_viewer/*.py ${PREFIX}/volumina_viewer
-echo "${PREFIX}" > ${PREFIX}/lib/python2.7/site-packages/volumina_viewer.pth
+echo "${PREFIX}" > ${PREFIX}/lib/python${PY_VER}/site-packages/volumina_viewer.pth
 python -m compileall ${PREFIX}/volumina_viewer
